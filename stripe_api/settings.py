04b7293f62903d749aab813b7e117f93e4342191
F70st <damir.serazetdinow@yandex.ru>
2022-09-19 14:32:16 +0300

    heroku changes

--- stripe_api/settings.py
+++ stripe_api/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'strpiapi.herokuapp.com']
 
 
 # Application definition
