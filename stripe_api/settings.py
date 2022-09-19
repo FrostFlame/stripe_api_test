a50ec0d7915dc6eaec68d5bf5cf88509f9dc77a9
F70st <damir.serazetdinow@yandex.ru>
2022-09-19 15:38:11 +0300

    heroku changes

--- stripe_api/settings.py
+++ stripe_api/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'strpiapi.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
